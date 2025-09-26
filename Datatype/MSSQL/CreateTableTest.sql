9a40b9b2edff3cd384b0eedbac220fb29cfa1a51
PierreAntoineAP <145551129+PierreAntoineAP@users.noreply.github.com>
2025-09-26 15:06:07 +0200

    Update CreateTableTest.sql

--- Datatype/MSSQL/CreateTableTest.sql
+++ Datatype/MSSQL/CreateTableTest.sql
@@ -1,66 +1,116 @@
 USE [FastTransfer-Tests];
 GO
 
+-- Création des schémas
+CREATE SCHEMA source;
+GO
+CREATE SCHEMA target;
+GO
+
 -- Uniqueidentifier
-CREATE TABLE Test_uniqueidentifier (
+CREATE TABLE source.Test_uniqueidentifier (
+    ID INT IDENTITY PRIMARY KEY,
+    Data UNIQUEIDENTIFIER
+);
+CREATE TABLE target.Test_uniqueidentifier (
     ID INT IDENTITY PRIMARY KEY,
     Data UNIQUEIDENTIFIER
 );
 
 -- Hierarchyid
-CREATE TABLE Test_hierarchyid (
+CREATE TABLE source.Test_hierarchyid (
+    ID INT IDENTITY PRIMARY KEY,
+    Data HIERARCHYID
+);
+CREATE TABLE target.Test_hierarchyid (
     ID INT IDENTITY PRIMARY KEY,
     Data HIERARCHYID
 );
 
 -- Geometry
-CREATE TABLE Test_geometry (
+CREATE TABLE source.Test_geometry (
+    ID INT IDENTITY PRIMARY KEY,
+    Data GEOMETRY
+);
+CREATE TABLE target.Test_geometry (
     ID INT IDENTITY PRIMARY KEY,
     Data GEOMETRY
 );
 
 -- Geography
-CREATE TABLE Test_geography (
+CREATE TABLE source.Test_geography (
+    ID INT IDENTITY PRIMARY KEY,
+    Data GEOGRAPHY
+);
+CREATE TABLE target.Test_geography (
     ID INT IDENTITY PRIMARY KEY,
     Data GEOGRAPHY
 );
 
 -- XML
-CREATE TABLE Test_xml (
+CREATE TABLE source.Test_xml (
+    ID INT IDENTITY PRIMARY KEY,
+    Data XML
+);
+CREATE TABLE target.Test_xml (
     ID INT IDENTITY PRIMARY KEY,
     Data XML
 );
 
 -- Varbinary(max)
-CREATE TABLE Test_varbinary (
+CREATE TABLE source.Test_varbinary (
+    ID INT IDENTITY PRIMARY KEY,
+    Data VARBINARY(MAX)
+);
+CREATE TABLE target.Test_varbinary (
     ID INT IDENTITY PRIMARY KEY,
     Data VARBINARY(MAX)
 );
 
 -- Nvarchar(max)
-CREATE TABLE Test_nvarchar (
+CREATE TABLE source.Test_nvarchar (
+    ID INT IDENTITY PRIMARY KEY,
+    Data NVARCHAR(MAX)
+);
+CREATE TABLE target.Test_nvarchar (
     ID INT IDENTITY PRIMARY KEY,
     Data NVARCHAR(MAX)
 );
 
 -- Sql_variant
-CREATE TABLE Test_sql_variant (
+CREATE TABLE source.Test_sql_variant (
+    ID INT IDENTITY PRIMARY KEY,
+    Data SQL_VARIANT
+);
+CREATE TABLE target.Test_sql_variant (
     ID INT IDENTITY PRIMARY KEY,
     Data SQL_VARIANT
 );
 
--- Dépréciés mais exotiques
-CREATE TABLE Test_image (
+-- Dépréciés
+CREATE TABLE source.Test_image (
+    ID INT IDENTITY PRIMARY KEY,
+    Data IMAGE
+);
+CREATE TABLE target.Test_image (
     ID INT IDENTITY PRIMARY KEY,
     Data IMAGE
 );
 
-CREATE TABLE Test_text (
+CREATE TABLE source.Test_text (
+    ID INT IDENTITY PRIMARY KEY,
+    Data TEXT
+);
+CREATE TABLE target.Test_text (
     ID INT IDENTITY PRIMARY KEY,
     Data TEXT
 );
 
-CREATE TABLE Test_ntext (
+CREATE TABLE source.Test_ntext (
+    ID INT IDENTITY PRIMARY KEY,
+    Data NTEXT
+);
+CREATE TABLE target.Test_ntext (
     ID INT IDENTITY PRIMARY KEY,
     Data NTEXT
 );
